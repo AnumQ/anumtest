d20ae147d91526f9b96c520391d379e92b21ef5e
anum <hello@anum.io>
2021-09-03 13:14:36 +0200

    Initial Commit

---
+++ anumtest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anumtest
+//
+//  Created by Anum Qudsia on 03/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ anumtest/anumtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  anumtestApp.swift
+//  anumtest
+//
+//  Created by Anum Qudsia on 03/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct anumtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
